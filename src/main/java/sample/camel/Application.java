80334255276f50f1a7030906af4bc03dce094e98
Luigi De Masi <ldemasi@redhat.com>
2023-05-05 16:56:47 +0200

    Add camel jmx metrics to prometheus exporter

--- src/main/java/sample/camel/Application.java
+++ src/main/java/sample/camel/Application.java
@@ -20,23 +20,36 @@ import io.micrometer.core.instrument.Clock;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.prometheus.client.CollectorRegistry;
+import io.prometheus.jmx.BuildInfoCollector;
+import io.prometheus.jmx.JmxCollector;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.micrometer.MicrometerConstants;
 import org.apache.camel.component.micrometer.eventnotifier.MicrometerExchangeEventNotifier;
 import org.apache.camel.component.micrometer.messagehistory.MicrometerMessageHistoryFactory;
 import org.apache.camel.component.micrometer.routepolicy.MicrometerRoutePolicyFactory;
 import org.apache.camel.spring.boot.CamelContextConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 
-//CHECKSTYLE:OFF
+
+import javax.management.MalformedObjectNameException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * A sample Spring Boot application that starts the Camel routes.
  */
 @SpringBootApplication
 public class Application {
+
+
 
     /**
      * A main method to start this application.
@@ -46,28 +59,41 @@ public class Application {
     }
 
 
-    @Bean(name = { MicrometerConstants.METRICS_REGISTRY_NAME, "prometheusMeterRegistry" })
+    @Bean(name = {MicrometerConstants.METRICS_REGISTRY_NAME, "prometheusMeterRegistry"})
     public PrometheusMeterRegistry prometheusMeterRegistry(
-            PrometheusConfig prometheusConfig, CollectorRegistry collectorRegistry, Clock clock) {
+            PrometheusConfig prometheusConfig, CollectorRegistry collectorRegistry, Clock clock) throws MalformedObjectNameException, IOException {
+
+        InputStream resource = new ClassPathResource("config/prometheus.yml").getInputStream();
+
+        new JmxCollector(resource).register(collectorRegistry);
+        new BuildInfoCollector().register(collectorRegistry);
         return new PrometheusMeterRegistry(prometheusConfig, collectorRegistry, clock);
     }
 
     @Bean
-    public CamelContextConfiguration camelContextConfiguration() {
+    public CamelContextConfiguration camelContextConfiguration(@Autowired PrometheusMeterRegistry registry) {
 
         return new CamelContextConfiguration() {
             @Override
             public void beforeApplicationStart(CamelContext camelContext) {
-                camelContext.addRoutePolicyFactory(new MicrometerRoutePolicyFactory());
-                camelContext.setMessageHistoryFactory(new MicrometerMessageHistoryFactory());
-                camelContext.getManagementStrategy().addEventNotifier(new MicrometerExchangeEventNotifier());
+                MicrometerRoutePolicyFactory micrometerRoutePolicyFactory = new MicrometerRoutePolicyFactory();
+                micrometerRoutePolicyFactory.setMeterRegistry(registry);
+                camelContext.addRoutePolicyFactory(micrometerRoutePolicyFactory);
+
+                MicrometerMessageHistoryFactory micrometerMessageHistoryFactory = new MicrometerMessageHistoryFactory();
+                micrometerMessageHistoryFactory.setMeterRegistry(registry);
+                camelContext.setMessageHistoryFactory(micrometerMessageHistoryFactory);
+
+                MicrometerExchangeEventNotifier micrometerExchangeEventNotifier =  new MicrometerExchangeEventNotifier();
+                micrometerExchangeEventNotifier.setMeterRegistry(registry);
+                camelContext.getManagementStrategy().addEventNotifier(micrometerExchangeEventNotifier);
             }
 
             @Override
             public void afterApplicationStart(CamelContext camelContext) {
-
             }
         };
     }
+
 }
-//CHECKSTYLE:ON
+
